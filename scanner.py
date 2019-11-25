449990af1eb22553050c561a4da8ecf38241e9e6
virdiignis <gbryk1@gmail.com>
2019-11-26 00:06:39 +0100

    bt presence scanner

---
+++ scanner.py
@@ -0,0 +1,15 @@
+import bluetooth
+
+
+class Scanner:
+    def __init__(self, mac):
+        self.__mac = mac
+
+    def __discover__(self):
+        self.__nearby_devices = bluetooth.discover_devices(duration=1, flush_cache=True, lookup_class=False)
+
+    def is_present(self):
+        for addr, name in self.__nearby_devices:
+            if addr == self.__mac:
+                return True
+        return False
